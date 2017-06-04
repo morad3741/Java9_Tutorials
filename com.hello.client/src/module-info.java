3ced1f88bb638c981c84ccca60561dd7c598b21a
morad3741 <moraddudi@gmail.com>
2017-06-04 19:59:24 +0200

    update

---
+++ com.hello.client/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by LD on 04/06/2017.
+ */
+module com.hello.client {
+
+    requires com.hello;
+
+}
---
+++ module1/src/com/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.hello;
+
+/**
+ * Created by LD on 04/06/2017.
+ */
+public class HelloWorld {
+}
